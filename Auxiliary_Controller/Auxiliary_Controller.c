f441feb3acd972d181125b2c9b23f401be4193f2
IseanB <44033533+IseanB@users.noreply.github.com>
2022-01-14 18:31:49 -0800

    Update Auxiliary_Controller.c

--- Auxiliary_Controller/Auxiliary_Controller.c
+++ Auxiliary_Controller/Auxiliary_Controller.c
@@ -6,12 +6,31 @@
 #include "hardware/watchdog.h"
 #include "hardware/clocks.h"
 
+uint relayState = relayState;
+
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
     return 0;
 }
 
+void gpioControl(int gpioNum, bool offOn) {
+  if(gpioNum > 0 && gpioNum < 35 && gpioNum != 3 && gpioNum != 8 && gpioNum != 13 && gpioNum != 18 && gpioNum != 23 && gpioNum != 28 && gpioNum != 33){
+      gpio_set_dir(gpioNumber, offOn);
+      sleep_ms(101);
+      relayState = gpio_get_dir(gpioNumber);
+  }
+  else{
+    relayState = -1;
+  }
+}
 
+
+//line 16: user inputs GPIO pin, int, and wheter they want to open channel, bool.
+//line 17: checks if GPIO pin is a valid pin
+//line 18: turns on/off the gpio pin depending on what the user wants, through bool parameter.
+//line 19: wait 101 ms for physical delay
+//line 20: updates global variable with dir direction, 1 for out, 0 for in.
+//line 23: if an invalid pin is inputed, the global variable is set to -1 to indicate an error.
 
 int main()
 {
