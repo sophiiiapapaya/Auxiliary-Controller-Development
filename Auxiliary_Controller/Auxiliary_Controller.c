209348ad01d38d19e6d4ade56407ffbdfb438177
5-Jeremy <jcarl023@ucr.edu>
2022-03-23 16:59:52 -0700

    Import code from relay-dev-with-pwm branch

--- Auxiliary_Controller/Auxiliary_Controller.c
+++ Auxiliary_Controller/Auxiliary_Controller.c
@@ -20,6 +20,32 @@ void Relay_Init();
 void Relay_Control(bool openClose);
 void Relay_Unit_Test();
 
+// Code from Isean and Justin
+const uint pwmPinInput = 27; //This pin is a PWM 2B type
+// Try using pin 5 if 27 doesn't work
+float measure_duty_cycle(uint gpio){
+      // Only the PWM B pins can be used as inputs.
+      assert(pwm_gpio_to_channel(gpio) == PWM_CHAN_B);//skips over function if pin is not pwm b 
+      uint slice_num = pwm_gpio_to_slice_num(gpio);// slices are used to measure frequency/duty cycles
+
+      // Count once for every 100 cycles the PWM B input is high
+      pwm_config cfg = pwm_get_default_config();//intitialization
+      pwm_config_set_clkdiv_mode(&cfg, PWM_DIV_B_HIGH);
+      pwm_config_set_clkdiv(&cfg, 100);
+      
+      pwm_init(slice_num, &cfg, false);
+      gpio_set_function(gpio, GPIO_FUNC_PWM);
+
+      pwm_set_enabled(slice_num, true);
+      sleep_ms(10);
+      pwm_set_enabled(slice_num, false);
+      float counting_rate = clock_get_hz(clk_sys) / 100;
+      float max_possible_count = counting_rate * 0.01;
+      return pwm_get_counter(slice_num) / max_possible_count;
+}
+
+// Alternate code based on 
+
 int main() {
     stdio_init_all();
     stdio_usb_init();
