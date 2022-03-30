62ca31f5c2ce8e55ba2021f468df9dc4cbb5099d
Maverick Bautista <mbaut030@ucr.edu>
2022-03-30 12:31:31 -0700

    Added message definitions, issue #5

--- Auxiliary_Controller/headers/gcs_messages.h
+++ Auxiliary_Controller/headers/gcs_messages.h
@@ -16,6 +16,30 @@ typedef struct GCS_msg {
         // a specific message does not matter as long as it is unique
     // num_bytes is simply the number of data bytes that the message uses
     // payload will be a pointer to an array of bytes which have been filled in with the required information
+//Relay for VTOL Motors
 #define RELAY_STATUS_MSG(payload) {.header = 0x80, \
+                                   .num_bytes = 1, \
+                                   .bytes = payload, }
+//UGV Deployment
+#define BAY_DOOR_STATUS_MSG(payload) {.header = 0x81, \
+                                   .num_bytes = 2, \
+                                   .bytes = payload, }
+#define DROP_STATUS_MSG(payload) {.header = 0x82, \
+                                   .num_bytes = 4, \
+                                   .bytes = payload, }
+//Diagnostic Messages
+#define SERVO_VOLTAGE_STATUS_MSG(payload) {.header = 0x83, \
+                                   .num_bytes = 1, \
+                                   .bytes = payload, }
+#define STEPPER_MOTOR_STATUS_MSG(payload) {.header = 0x84, \
+                                   .num_bytes = 1, \
+                                   .bytes = payload, }
+#define RELAY_POWER_STATUS_MSG(payload) {.header = 0x85, \
+                                   .num_bytes = 1, \
+                                   .bytes = payload, }
+#define CONTROLLER_STATUS_MSG(payload) {.header = 0x86, \
+                                   .num_bytes = 2, \
+                                   .bytes = payload, }
+#define COMMDAN_RECEIVED_MSG(payload) {.header = 0x87, \
                                    .num_bytes = 1, \
                                    .bytes = payload, }
